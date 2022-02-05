77cb3977789bbd8c8fabbba663cb74b6eb50df14
acamp <acampbell8610@gmail.com>
2022-02-04 21:50:10 -0600

    updated displayartistblock, api keys, form submit handler
    created form submit handler for button and adjusted html for it
    
    created the beginning part of the display div
    
    added the multiple api keys

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,33 @@
+<<<<<<< Updated upstream
 fetch("https://spotify23.p.rapidapi.com/search/?q=adele&type=multi&offset=0&limit=10&numberOfTopResults=5", {
+=======
+var srchBtnEL = document.getElementById("srch-btn");
+var searchInputEl = document.getElementById("search-input");
+var artistBlockEl = document.querySelector("#artist-block");
+
+function clickMe() {
+	
+}
+
+function spotifyApi() {
+var artistName= searchInputEl.value.trim()
+console.log(artistName);
+
+	const secretKey = 'mumaKeySpotify' //Enter a unique string for each fetch that usees a different set of keys
+    const keys = ['336db5670cmshe924e8d60e474d5p15ce38jsnff37f2a4c07d','4df9de0ba0msh15d940754aa44e0p19aa82jsn9517ca64824b','cecd3c5aa0mshbeb36b25441b59fp19a4d5jsne893d85ca06c','32633583a2msh6023211a7ca5fc3p11b52fjsnbe86d190dff9']; //Your actual API Key at each index for each amount of keys
+    const maxCalls = 100 //someInteger of Max calls for API
+    const expirationHours = 24
+    const keyCount = localKeyCount(keys.length, maxCalls, expirationHours, secretKey) //initializes keyCount with localStorage keyCount object
+
+    if (pickBestKey(keyCount) == -1) {
+        return console.log('Out of key calls. Clear local storage to end this message.')
+    }
+
+    let apiURL = `https://testURL.com/coding/is/fun/?q=answerKey&apiKey=${keys[pickBestKey(keyCount)]}` //picks best key from array of keys based on highest remaining calls
+
+
+fetch(`https://spotify23.p.rapidapi.com/search/?q=${artistName}&type=multi&offset=0&limit=10&numberOfTopResults=5`, {
+>>>>>>> Stashed changes
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "spotify23.p.rapidapi.com",
@@ -6,12 +35,212 @@ fetch("https://spotify23.p.rapidapi.com/search/?q=adele&type=multi&offset=0&limi
 	}
 })
 .then(response => {
+<<<<<<< Updated upstream
     return response.json() 
+=======
+	updateKeyCount(pickBestKey(keyCount), keyCount, secretKey)
+	console.log(keyCount)
+    return response.json().then(function(data){
+		console.log(data)
+	}) 
+>>>>>>> Stashed changes
 
 }).then(data => {
     console.log(data)
+
+	diplayArtistBlock(data);
+
+
 })
-.catch(err => {
-	console.error(err);
+.catch(function(error){
+	alert("Uable to connect to the API");
 });
 
+<<<<<<< Updated upstream
+=======
+}
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    var artistSearch= searchInputEl.value.trim()
+    ;
+    localStorage.setItem("artistSearch",artistSearch);
+   
+console.log(artistSearch)
+    if(artistSearch){
+        spotifyApi();
+       
+     }
+     else{
+         alert("Please Enter an Artist");
+     }
+     console.log(event);
+     
+};
+
+function diplayArtistBlock(){
+	artistBlockEl.textContent="";
+	var searchResults=document.createElement("div")
+	searchResults.classList="search-results"
+
+	var artistName= document.createElement("h3");
+	artistName.textContent= (data.artist.items[0].data.profile.name);
+
+	var artistImgEl=document.createElement("img");
+	artistImgEl= (data.user.items[0].data.img.smallImageUrl)
+
+	 for(var i=0; i < 10; i++){
+		var coverArtEl= document.createElement("img");
+		coverArtEl.src = (data.albums.items[i].data.coverArt.sources[0].url)
+
+		searchResults.appendChild(coverArtEl);
+		
+	 }
+	 artistBlockEl.appendChild(artistName);
+	 artistBlockEl.appendChild(artistImgEl);
+	 artistBlockEl.appendChild(searchResults);
+
+}
+
+
+srchBtnEL.addEventListener("click", formSubmitHandler);
+
+
+//hamzahs API key code 
+
+
+//initializes localStorage with keyCount
+function localKeyCount(keyLength, calls, expirationHours, secretKey) {
+    let keyValues = ''
+    let dateToday = (Date.parse(Date()))
+    let dateScale = (expirationHours * 3600 * 1000)
+    let currentDate = new Date()
+
+    if (localStorage.getItem(`keyCount${secretKey}`) == null || localStorage.getItem(`createdAt${secretKey}`) == null) {
+        keyValues = populateKeyCount(keyLength, calls);
+        localStorage.setItem(`keyCount${secretKey}`, JSON.stringify(keyValues))
+        localStorage.setItem(`createdAt${secretKey}`, dateToday)
+
+        console.log(`Key calls created on ${currentDate.toLocaleDateString()} at ${currentDate.toLocaleTimeString()}`)
+
+        return localKeyCount(keyLength, calls, expirationHours, secretKey)
+    } else {
+        let value = localStorage.getItem(`keyCount${secretKey}`)
+        let time = parseInt(localStorage.getItem(`createdAt${secretKey}`))
+        let date = new Date(time + dateScale)
+
+        if (dateToday > (dateScale + time)) {
+            localStorage.removeItem(`createdAt${secretKey}`);
+            localStorage.removeItem(`keyCount${secretKey}`)
+
+            return localKeyCount(keyLength, calls, expirationHours, secretKey)
+        }
+        console.log(`Your keys will refresh on ${date.toLocaleDateString()} at ${date.toLocaleTimeString()}`)
+        keyValues = JSON.parse(value)
+
+        return keyValues
+    }
+}
+
+//Populate keys with maxCall amount from localKeyCount
+function populateKeyCount(arrLength, calls) {
+    let arr = new Array(arrLength)
+    let uniqueArr = []
+    let i = 0;
+    while (uniqueArr.length < arrLength) {
+        uniqueArr.push(getRandom(10000))
+        if (uniqueArr.indexOf(uniqueArr[i]) == -1) {
+            i++
+        } else {
+            uniqueArr[i] = getRandom(10000)
+        }
+    }
+    for (var j = 0; j < arrLength; j++) {
+        arr[j] = ([getRandom(1, 9999), calls])
+    }
+
+    return Object.fromEntries(arr)
+};
+
+//Best best key with most calls remaining
+function pickBestKey(keyCount) {
+    let keyObj = Object.entries(keyCount)
+    let bestKey = keyObj[0]
+    let bestKeyIndex = 0;
+
+    keyObj.forEach((element, index) => {
+        if (element[1] > bestKey[1]) {
+            bestKey = element
+            bestKeyIndex = index
+        }
+    })
+
+    if (bestKey[1] == 0) {
+        return -1
+    } else {
+        return bestKeyIndex
+    }
+};
+
+//updates localStorage with 
+function updateKeyCount(keyIndex, keyCount, secretKey) {
+    let keyArr = Object.entries(keyCount)
+
+    keyArr[keyIndex][1]--
+
+    return localStorage.setItem(`keyCount${secretKey}`, JSON.stringify(Object.fromEntries(keyArr)))
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function fetchAPI() {
+    const secretKey = 'mumaKey1' //Enter a unique string for each fetch that usees a different set of keys
+    const keys = ['trueKey1', 'trueKey2', 'trueKey3', 'trueKey4']; //Your actual API Key at each index for each amount of keys
+    const maxCalls = 100 //someInteger of Max calls for API
+    const expirationHours = 24
+    const keyCount = localKeyCount(keys.length, maxCalls, expirationHours, secretKey) //initializes keyCount with localStorage keyCount object
+
+    if (pickBestKey(keyCount) == -1) {
+        return console.log('Out of key calls. Clear local storage to end this message.')
+    }
+
+    let apiURL = `https://testURL.com/coding/is/fun/?q=answerKey&apiKey=${keys[pickBestKey(keyCount)]}` //picks best key from array of keys based on highest remaining calls
+
+    /*After fetch(apiURL).then(response => {
+        if(response.ok){*/
+    updateKeyCount(pickBestKey(keyCount), keyCount, secretKey)
+    /*      return response.json()  
+}
+}) */
+
+    console.log(keyCount) //Objest of calls left for each Key 
+};
+
+function fetchAPI2() {
+    const secretKey = 'mumaKey2' //Enter a unique string for each fetch that usees a different set of keys
+    const keys = ['trueKey1', 'trueKey2', 'trueKey3', 'trueKey4']; //Your actual API Key at each index for each amount of keys
+    const maxCalls = 100 //someInteger of Max calls for API
+    const expirationHours = 24
+    const keyCount = localKeyCount(keys.length, maxCalls, expirationHours, secretKey) //initializes keyCount with localStorage keyCount object
+
+    if (pickBestKey(keyCount) == -1) {
+        return console.log('Out of key calls. Clear local storage to end this message.')
+    }
+
+    let apiURL = `https://testURL.com/coding/is/fun/?q=answerKey&apiKey=${keys[pickBestKey(keyCount)]}` //picks best key from array of keys based on highest remaining calls
+
+    /*After fetch(apiURL).then(response => {
+        if(response.ok){*/
+    updateKeyCount(pickBestKey(keyCount), keyCount, secretKey)
+    /*      return response.json()  
+}
+}) */
+
+    console.log(keyCount) //Objest of calls left for each Key 
+};
+
+//Your API Call
+// spotifyApi()
+
+>>>>>>> Stashed changes
